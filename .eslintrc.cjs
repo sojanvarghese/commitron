b554fd5b20346f7f84cd6c72b9a2655094337840
Sojan Varghese <sojanvarghese@Sojans-MacBook-Air.local>
2025-09-06 14:36:26 +0530

    feat(general): Added ESLint configuration file

---
+++ .eslintrc.cjs
@@ -0,0 +1,29 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2022,
+    sourceType: 'module',
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  plugins: [
+    '@typescript-eslint',
+  ],
+  rules: {
+    '@typescript-eslint/no-unused-vars': 'error',
+    '@typescript-eslint/no-explicit-any': 'warn',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-empty-function': 'off',
+    'no-console': 'off',
+    'prefer-const': 'error',
+    'no-var': 'error',
+  },
+  env: {
+    node: true,
+    es2022: true,
+  },
+  ignorePatterns: ['dist/', 'node_modules/'],
+};
