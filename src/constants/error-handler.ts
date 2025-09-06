1ccbda169d69195028a54f801a3ac9a61b17ed62
Sojan Varghese <sojanvarghese@Sojans-MacBook-Air.local>
2025-09-07 01:01:55 +0530

    Improved error handler constants

---
+++ src/constants/error-handler.ts
@@ -0,0 +1,8 @@
+/**
+ * Error handler related constants
+ */
+
+export const ERROR_LOG_LIMIT = 100;
+export const RECENT_ERROR_THRESHOLD_MS = 24 * 60 * 60 * 1000; // 24 hours
+export const DEFAULT_RETRY_ATTEMPTS = 3;
+export const DEFAULT_RETRY_DELAY_MS = 500;
